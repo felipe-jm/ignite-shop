637491126a8b708fd54806c2a64e46f299faa8f1
Felipe Jung <felipemattoseu@gmail.com>
2024-09-06 12:36:37 -0400

    Add next Image for logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
-import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
+import Image from "next/image";
+
+import { globalStyles } from "@/styles/global";
+
+import { Container, Header } from "@/styles/pages/app";
 
 import logoImg from "../assets/logo.svg";
-import { Container, Header } from "@/styles/pages/app";
 
 globalStyles();
 
@@ -10,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg.src} alt="" />
       </Header>
 
       <Component {...pageProps} />

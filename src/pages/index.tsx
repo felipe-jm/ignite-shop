450b8874d527ec9c00c5ec5a020e106f34d7836c
Felipe Jung <felipemattoseu@gmail.com>
2024-09-10 12:31:29 -0400

    Add products carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
-import { HomeContainer, Product } from "@/styles/pages/home";
 import Image from "next/image";
+
+import { useKeenSlider } from "keen-slider/react";
+
+import { HomeContainer, Product } from "@/styles/pages/home";
 
 import camiseta1 from "../assets/camisetas/1.png";
 import camiseta2 from "../assets/camisetas/2.png";
 import camiseta3 from "../assets/camisetas/3.png";
 
+import "keen-slider/keen-slider.min.css";
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
     <>
-      <HomeContainer>
-        <Product>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        <Product className="keen-slider__slide">
           <Image src={camiseta1} alt="" height={520} width={480} />
 
           <footer>
@@ -18,8 +30,17 @@ export default function Home() {
           </footer>
         </Product>
 
-        <Product>
+        <Product className="keen-slider__slide">
           <Image src={camiseta2} alt="" height={520} width={480} />
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+
+        <Product className="keen-slider__slide">
+          <Image src={camiseta3} alt="" height={520} width={480} />
 
           <footer>
             <strong>Camiseta X</strong>

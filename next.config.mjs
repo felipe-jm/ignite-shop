c8d9bbd342d26a15dce6078784a4a47d8dce2cf8
Felipe Jung <felipemattoseu@gmail.com>
2024-09-12 12:20:22 -0400

    Add stripe products listing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  images: {
+    remotePatterns: [{ hostname: "files.stripe.com" }],
+  },
 };
 
 export default nextConfig;

6ae5508d439f37d12a3be76cf7f141c3276c2228
Felipe Jung <felipemattoseu@gmail.com>
2024-09-26 13:06:11 -0400

    Add success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,19 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
   return (
-    <>
-      <h1>Success</h1>
-    </>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Felipe Jung</strong>, sua <strong>Camisa</strong> já está
+        a caminho da sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
   );
 }
